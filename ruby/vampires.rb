b5e5380c2fccb4f6fff95af56ede249d36d9f220
Jeffrey Kranking <jkranking@gmail.com>
2016-10-04 16:24:13 -0700

    Initial commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,21 @@
+
+
+puts "What is your name?"
+
+name = gets.chomp
+
+puts "How old are you?"
+
+age = gets.chomp
+
+puts "What year were you born?"
+
+birth_year = gets.chomp
+
+puts "Should we order you garlic bread?"
+
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+
+insurance = gets.chomp
