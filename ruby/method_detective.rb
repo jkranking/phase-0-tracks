83bb0569c54aeeacd8d7c841d9c17998d6cebe8c
Jeffrey Kranking <jkranking@gmail.com>
2016-10-11 10:50:27 -0700

    Finish 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,40 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1, "o")
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
-
-# " suspects".<???>
+"the usual".insert 9, "suspects"
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+" suspects".insert 0, "the usual"
+# => "the usual suspects"
+
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+# couldn't find a method for this one so just returned the string without [0]
+
+"The mystery of the missing first letter".[1..-1]     
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# 122 is the ASCII (American Standard Code for Information Interchange) Code for the character z.
+
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
