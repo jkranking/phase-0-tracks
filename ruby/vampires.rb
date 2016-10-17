776fbe268245b3c870ea3a12a10ffacbe0a8d9e3
Jeffrey Kranking <jkranking@gmail.com>
2016-10-16 18:23:49 -0700

    Clean tabbing

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,91 +1,71 @@
 
 puts "How many employees will be processed?"
 
-  employees = gets.chomp.to_i
-
-  number_of_employees = 0
+employees = gets.chomp.to_i
+number_of_employees = 0
 
 until number_of_employees == employees
 
   puts "What is your name?"
-
-    name = gets.chomp
+  name = gets.chomp
 
   puts "How old are you?"
-
-    age = gets.chomp.to_i
+  age = gets.chomp.to_i
 
   puts "What year were you born?"
+  birth_year = gets.chomp.to_i
 
-    birth_year = gets.chomp.to_i
-
-    if 2016 - birth_year == age
-
-      correct_age = true 
-
-    end 
+  if 2016 - birth_year == age
+    correct_age = true 
+  end 
 
   puts "Should we order you garlic bread? Yes or No"
+  garlic_bread = gets.chomp.downcase
 
-    garlic_bread = gets.chomp.downcase
-
-    if garlic_bread == "yes" 
-
-      willing_to_eat_garlic = true 
-
-    end 
+  if garlic_bread == "yes" 
+    willing_to_eat_garlic = true 
+  end 
 
   puts "Would you like to enroll in the company's health insurance? Yes or No"
 
-    insurance = gets.chomp.downcase
+  insurance = gets.chomp.downcase
 
-    if insurance == "yes"
-
-      wants_insurance = true 
-
-    end 
+  if insurance == "yes"
+    wants_insurance = true 
+  end 
 
 
   number_of_allergies = true 
 
   until number_of_allergies == false 
-
     puts "Do you have any allergies? If so type the allergy one at a time and hit enter. Type done if finished"
-
     allergy = gets.chomp
 
     if allergy == "done" || allergy == "sunshine"
       number_of_allergies = false 
-
     end 
-
   end 
 
 
   if name == "Tu Fang" || name == "Drake Cula"
-
     puts "The employee is clearly a vampire"
 
-
-  elsif correct_age == true && (willing_to_eat_garlic == true || wants_insurance == true) 
-
+    elsif correct_age == true && (willing_to_eat_garlic == true || wants_insurance == true) 
     puts "The employee is probably not a vampire"
 
-  elsif correct_age != true && willing_to_eat_garlic != true && wants_insurance != true
-
+    elsif correct_age != true && willing_to_eat_garlic != true && wants_insurance != true
     puts "The employee is almost certainly a vampire"
 
-  elsif correct_age != true && (willing_to_eat_garlic != true || wants_insurance != true)
-
+    elsif correct_age != true && (willing_to_eat_garlic != true || wants_insurance != true)
     puts "The employee is probably a vampire"
 
-  else puts "Results inconclusive"
-
+    else puts "Results inconclusive"
+      
   end
 
-number_of_employees += 1
+  number_of_employees += 1
 
-puts 
+  puts 
 
 end 
 
