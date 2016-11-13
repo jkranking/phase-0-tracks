51952a60c05fff0d8930e7e5ef0ac9e2de925ed3
Jeffrey Kranking <jkranking@gmail.com>
2016-11-13 09:25:13 -0800

    Finishes Sinatra App

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,4 +1,3 @@
-# require gems
 require 'sinatra'
 require 'sqlite3'
 
@@ -7,25 +6,40 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
-# show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
   erb :home
 end
 
-get '/students/new' do
+get '/:id' do
+  @student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+  erb :show
+end 
+
+get '/student_new' do
   erb :new_student
 end
 
-# create new students via
-# a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
 
-# add static resources
+get '/update_student/:id' do
+  @student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+  erb :update_student
+end
 
+post '/update/:id' do
+  db.execute("UPDATE students SET name=?, campus=?, age=? WHERE id=?", [params['name'], params['campus'], params['age'].to_i], [params[:id]])
+  redirect '/'
+end
+
+get '/delete/:id' do 
+  db.execute("DELETE FROM students WHERE id=?", [params[:id]])
+  redirect '/'
+end 
+  
 get '/student_search' do 
   name = params['name']
   if name 
@@ -36,11 +50,7 @@ get '/student_search' do
         name_array.push(row)
       end
     end 
-    @name_array = name_array[0]
+    @name_array = name_array
   end 
   erb :search_student
 end 
-
-
-
-# Add an ERB template to the application in phase-0-tracks/web_dev/sinatra_templating. It can be relevant to the other pages or something totally different, but it should use at least one dynamic piece of data (which you'll populate in the next release).
